7f594758926f91625fc7a3829bdbb2827513a1ab
Bryan Recinos <87162291+brecinosm@users.noreply.github.com>
2023-10-22 14:00:48 -0600

    Add files via upload

---
+++ Proyecto_Analisis_II/src/proyecto_Analisis/Proyecto_Analisis.java
@@ -0,0 +1,15 @@
+package proyecto_Analisis;
+// @author Bryan Recinos
+
+import proyecto_Analisis.Vista.Formulario;
+
+public class Proyecto_Analisis {
+
+    public static void main(String[] args) {
+
+        Formulario form = new Formulario();
+        form.setVisible(true);
+
+    }
+
+}
